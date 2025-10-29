4a078c6d77b9dc4ca148bb2ef9f539c5160acc58
ThanThor1 <vuxuandungnb2k5@gmail.com>
2025-10-29 07:55:57 +0700

    Unit4_Pathway2_CodeLab_Cupcake

---
+++ Unit4_Pathway2_CodeLab_Cupcake/app/src/androidTest/java/com/example/cupcake/test/CupcakeOrderScreenTest.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.cupcake.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.example.cupcake.R
+import com.example.cupcake.ui.SelectOptionScreen
+import org.junit.Rule
+import org.junit.Test
+
+class CupcakeOrderScreenTest {
+
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+
+    @Test
+    fun selectOptionScreen_verifyContent() {
+        // Given list of options
+        val flavors = listOf("Vanilla", "Chocolate", "Hazelnut", "Cookie", "Mango")
+        // And subtotal
+        val subtotal = "$100"
+
+        // When SelectOptionScreen is loaded
+        composeTestRule.setContent {
+            SelectOptionScreen(subtotal = subtotal, options = flavors)
+        }
+
+        // Then all the options are displayed on the screen.
+        flavors.forEach { flavor ->
+            composeTestRule.onNodeWithText(flavor).assertIsDisplayed()
+        }
+
+        // And then the subtotal is displayed correctly.
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.subtotal_price,
+                subtotal
+            )
+        ).assertIsDisplayed()
+
+        // And then the next button is disabled
+        composeTestRule.onNodeWithStringId(R.string.next).assertIsNotEnabled()
+    }
+
+
+}
